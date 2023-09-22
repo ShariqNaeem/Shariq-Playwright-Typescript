de364efa2b9bcbed11662fe48c53c9318800e117
shariq-naeem-s37 <muhammadshariq.64@gmail.com>
2023-09-23 03:46:20 +0500

    completed the api test cases

--- tests/api/bookStore.spec.ts
+++ tests/api/bookStore.spec.ts
@@ -2,39 +2,62 @@ import { test, expect, request } from '@playwright/test'
 import apiData from '../../test-data/api-data.json'
 import { getRandomString } from '../../utils/randomString'
 import { callPostRequest } from '../../utils/postRequest'
+import { callDeleteRequest } from '../../utils/deleteRequest'
 
 test.describe.configure({ mode: 'serial' })
+const userEndPoint = '/Account/v1/user'
+const addBooksEndPoint = '/BookStore/v1/Books'
+const deleteBookEndPoint = '/BookStore/v1/Book'
 
 const username = getRandomString()
 const password = apiData.login.password
 const isbn = '9781593277574';
 let userId: string
-let accessToken: string
-let credentialsBase64: string
 
-test('Creation of user account', async ({ baseURL }) => {
-    const userEndPoint = '/Account/v1/user'
+// MAKING THE BASIC AUTH TOKEN
+const btoa = (str: string) => Buffer.from(str).toString('base64');
+const credentialsBase64 = btoa(`${username}:${password}`);
+const authHeader = { 'Authorization': `Basic ${credentialsBase64}` };
+
+test('1. Creation of user account', async ({ baseURL }) => {
     const requestCall = await request.newContext()
+    let body = {
+        userName: username,
+        password: password
+    }
+    const userResponse = await callPostRequest(requestCall, `${baseURL}${userEndPoint}`, body)
+    const content = await userResponse.json()
 
-    // MAKING THE BASIC AUTH TOKEN
-    const btoa = (str: string) => Buffer.from(str).toString('base64');
-    credentialsBase64 = btoa(`${username}:${password}`);
+    userId = content['userID']
+
+    expect(userResponse.status()).toBe(201)
+    expect(content).toHaveProperty('userID')
+    expect(typeof content.userID).toBe('string')
+    expect(content).toHaveProperty('username')
+    expect(typeof content.username).toBe('string')
+    expect(content.username).toEqual(username)
+    expect(content).toHaveProperty('books')
+    expect(Array.isArray(content.books)).toBe(true);
+})
+
+test('2. Try to create another user with same credentials', async ({ baseURL }) => {
+    const requestCall = await request.newContext()
     let body = {
         userName: username,
         password: password
     }
 
-    // CREATING THE USER
     const userResponse = await callPostRequest(requestCall, `${baseURL}${userEndPoint}`, body)
-    expect(userResponse.status()).toBe(201)
     const content = await userResponse.json()
-    userId = content['userID']
-    console.log(userId)
 
+    expect(userResponse.status()).toBe(406)
+    expect(content).toEqual({
+        code: '1204',
+        message: 'User exists!'
+    });
 })
 
-test('Add a list of books', async ({ baseURL }) => {
-    const addBooksEndPoint = '/BookStore/v1/Books'
+test('3. Add a list of books', async ({ baseURL }) => {
     const requestCall = await request.newContext()
     let body = {
         userId: userId,
@@ -44,12 +67,53 @@ test('Add a list of books', async ({ baseURL }) => {
             }
         ]
     }
-    const authHeader = { 'Authorization': `Basic ${credentialsBase64}` };
-
     const response = await callPostRequest(requestCall, `${baseURL}${addBooksEndPoint}`, body, authHeader)
     const content = await response.json()
     expect(response.status()).toBe(201)
-    expect
-    console.log(content)
+    expect(content).toHaveProperty('books')
+    expect(Array.isArray(content.books)).toBe(true)
+    expect(content.books[0]).toHaveProperty('isbn')
+    expect(content.books[0].isbn).toEqual(isbn)
+})
 
+test('4. Adding the same book for the same user', async ({ baseURL }) => {
+    const requestCall = await request.newContext()
+    let body = {
+        userId: userId,
+        collectionOfIsbns: [
+            {
+                isbn: isbn
+            }
+        ]
+    }
+    const response = await callPostRequest(requestCall, `${baseURL}${addBooksEndPoint}`, body, authHeader)
+    const content = await response.json()
+    expect(content).toEqual({
+        code: "1210",
+        message: "ISBN already present in the User's Collection!"
+    });
+})
+
+test('5. Remove one of the added books', async ({ baseURL }) => {
+    const requestCall = await request.newContext()
+    let body = {
+        userId: userId,
+        isbn: isbn
+    }
+    const response = await callDeleteRequest(requestCall, `${baseURL}${deleteBookEndPoint}`, body, authHeader)
+    expect(response.status()).toBe(204)
+})
+
+test('6. Try to remove book without basic auth token', async ({ baseURL }) => {
+    const requestCall = await request.newContext()
+    let body = {
+        userId: userId,
+        isbn: isbn
+    }
+    const response = await callDeleteRequest(requestCall, `${baseURL}${deleteBookEndPoint}`, body)
+    const content = await response.json()
+    expect(content).toEqual({
+        code: "1200",
+        message: "User not authorized!"
+    });
 })
